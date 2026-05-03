28d2473e6cfb90dbbaa713cb2c8d8dbdb3a00794
joao sergio <joaosergio179@gmail.com>
2026-05-02 23:20:38 -0400

    adicionei heranca de registro clinico em medicao

--- src/model/entity/Medicacao.java
+++ src/model/entity/Medicacao.java
@@ -2,17 +2,9 @@ package model.entity;
 
 import java.sql.Date;
 
-public class Medicacao {
-  private int id;
-  private Produto produto;
-  private Funcionario funcionario;
-  private Animal animal;
-  private Date data;
+public class Medicacao extends RegistroClinico{
 
-  public Medicacao(Produto produto, Funcionario funcionario, Animal animal, Date data){
-    this.produto = produto;
-    this.funcionario = funcionario;
-    this.animal = animal;
-    this.data = data;
+  public Medicacao(Produto produto, Funcionario veterinario, Animal animal, Date data){
+    super(veterinario, animal, produto, data);
   }
 }
