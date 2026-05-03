0c94ab5a7d53d6676d738359be2505aeca5ebe65
joao sergio <joaosergio179@gmail.com>
2026-05-02 23:30:18 -0400

    adicionei o metodo adicionarAtendimento e o atributo atendimentos na classe tutor

--- src/model/entity/Tutor.java
+++ src/model/entity/Tutor.java
@@ -1,20 +1,28 @@
 package model.entity;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 
 //estou usando herança pq nao estava fazendo sentido nao usar herença. da forma q estava basicamemente esta duplicando o cadastro, entao um tutor teria um id e a pessoa ligada a esse tutor teria outro id. 
 public class Tutor extends Pessoa {
   private boolean tutorAtivo;
+  private List<Atendimento> atendimentos;
 
   public Tutor(String cpf, String nome, Date dataNascimento, String endereco) {
     super(cpf, nome, dataNascimento, endereco); // aqui esta chamando o construtor (acho q é esse o nome )da classe pai (Pessoa) - tipo a funcao principal la da classe principal.
 
     this.tutorAtivo = true;
+    this.atendimentos = new ArrayList<>();
   }
 
   public void desativarTutor(){
     tutorAtivo = false;
+  }
+
+  public void adicionarAtendimento (Atendimento atendimento){
+    this.atendimentos.add(atendimento);
   }
 
 
