60a432283eb07f708c3b4466878659271c050689
joao sergio <joaosergio179@gmail.com>
2026-05-19 20:23:04 -0400

    adicionei um menu na main

--- src/main/App.java
+++ src/main/App.java
@@ -1,12 +1,46 @@
 package main;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
 
 import model.entity.*;
 import model.enums.*;
 
 public class App {
+    
     public static void main(String[] args) throws Exception {
+        String[] opcoesMenu = {"Marcar consulta", "Consultar exame", "Acessar portal do funcionario"};
+
+        System.out.println("\n=======================================");
+        System.out.println("      SISTEMA HOSPITAL VETERINÁRIO     ");
+        System.out.println("=======================================\n");
+
+        System.out.println("Seja bem-vindo(a) ao hospital. Como posso ajudar hoje?");
+
+        for(int i = 0; i<opcoesMenu.length; i++){
+
+            System.out.println(String.valueOf(i+1) + " - " + opcoesMenu[i] + ";");
+
+        }
+
+        System.out.println("Escolha uma opção [digite apenas o numero]: ");
+        Scanner leitor = new Scanner(System.in);
+        int opcaoSelecionada = leitor.nextInt();
+        leitor.close();
+
+        switch(opcaoSelecionada){
+            case 1: 
+            break;
+
+            case 2: 
+            break;
+
+            case 3:
+            break;
+        }
+
+
        
        // private Date dataNascimento = 20/04/2016
 
