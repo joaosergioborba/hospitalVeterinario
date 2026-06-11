61d1bbcfbb950d79e8af04194bdad744729ad70a
joao sergio <joaosergio179@gmail.com>
2026-06-11 15:33:18 -0400

    crei a primeira parte da tela de funcionarios

--- src/model/view/TelaFuncionario.java
+++ src/model/view/TelaFuncionario.java
@@ -1,5 +1,15 @@
 package model.view;
+import javax.swing.*;
+import model.service.ClinicaVeterinaria;
+import model.view.abstract_class.PopUpModel;
 
-public class TelaFuncionario {
-  
+public class TelaFuncionario extends JPanel{
+  private ClinicaVeterinaria clinicaVeterinaria;
+
+  public TelaFuncionario(ClinicaVeterinaria clinicaVeterinaria){
+    
+   this.clinicaVeterinaria = clinicaVeterinaria;
+
+  }
+
 }
